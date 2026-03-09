53359c2913d2af3969c842d294c0d757fd8f6bef
Jbamidi <jassubamidi24@gmail.com>
2026-03-09 00:14:11 -0500

    Message Type State Completed

--- rtl/itch_decoder.sv
+++ rtl/itch_decoder.sv
@@ -14,16 +14,17 @@ module itch_decode
     output logic        frame_accepted 
 );
 
-    typedef enum logic {
+    typedef enum logic [1:0] {
         s_type,
         s_collect
+        s_drop
     } state_t;
 
     state_t state, state_next;
 
     // Byte Counter
-    logic [2:0] byte_cnt;       
-    logic [2:0] byte_cnt_next;
+    logic [5:0] byte_cnt;       
+    logic [5:0] byte_cnt_next;
     logic [7:0] msgtype;
     logic [7:0] msgtype_next;
     logic [47:0] timestamp;
@@ -49,8 +50,73 @@ module itch_decode
 
     // Next-State Logic
     always_comb begin
+        state_next     = state;
+        byte_cnt_next  = byte_cnt;
+        msgtype_next = msg_type;
+        timestamp_next = timestamp_next;
 
+        m_axis_tdata   = 8'd0;
+        m_axis_tvalid  = 1'b0;
+        m_axis_tlast   = 1'b0;
+        s_axis_tready  = 1'b0;
+
+        frame_dropped  = 1'b0;
+        frame_accepted = 1'b0;
+        case (state)
+            s_type: begin
+                s_axis_tready = 1'b1;
+
+                if (accept) begin
+                    if (byte_cnt == 6'd0) begin
+                        msg_type = s_axis_tdata;
+                    end           
+                    if (s_axis_tlast) begin
+                        byte_cnt_next = 6'd0;
+                        state_next    = s_receive;
+                        frame_dropped = 1'b1;
+                    end
+                    else if (byte_cnt == 3'd7) begin
+                        if (udptype == 16'd26400) begin
+                            state_next     = s_payload;
+                            frame_accepted = 1'b1;
+                        end else begin
+                            state_next    = s_drop;
+                            frame_dropped = 1'b1;
+                        end
+                        byte_cnt_next = 3'd0;
+                    end e
+                    else begin
+                        byte_cnt_next = byte_cnt + 3'd1;
+                    end
+
+                end
+
+            s_collect: begin
+                m_axis_tdata  = s_axis_tdata;
+                m_axis_tvalid = s_axis_tvalid;
+                m_axis_tlast  = s_axis_tlast;
+                s_axis_tready = m_axis_tready; //Backpressure
+
+                if (accept && s_axis_tlast) begin
+                    state_next = s_receive;
+                end
+            end
+
+
+            s_drop: begin
+                s_axis_tready = 1'b1;
+
+                if (accept && s_axis_tlast) begin
+                    state_next = s_receive;
+                end
+            end
+
+            default: begin
+                state_next = s_receive;
+            end
+        endcase
     end
+
 
 
 endmodule
